4af4faf5a1fc4dcf4faab94ee07095471fc5c385
Fastermanboy <fab377@hotmail.com>
2026-04-07 15:27:56 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -13,35 +13,3 @@ const firebaseConfig = {
   messagingSenderId: "857827909082",
   appId: "1:857827909082:web:1ecc8775bccb605534b4e3"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-import { db } from "./firebase-config.js";
-import { ref, set } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-
-const workspace = "savigny-le-temple";
-
-// Création des catégories
-set(ref(db, `workspaces/${workspace}/categories`), {
-  ecriture: "Écriture",
-  rangement: "Rangement",
-  informatique: "Informatique"
-});
-
-// Exemple de fournitures
-set(ref(db, `workspaces/${workspace}/fournitures/id1`), {
-  nom: "Stylo bleu",
-  categorie: "ecriture",
-  stock: 12,
-  seuil: 5,
-  description: "Stylo bille bleu"
-});
-
-set(ref(db, `workspaces/${workspace}/fournitures/id2`), {
-  nom: "Classeur A4",
-  categorie: "rangement",
-  stock: 4,
-  seuil: 2,
-  description: "Classeur cartonné A4"
-});
--- firebase-init.js
+++ firebase-init.js
@@ -1,5 +1,28 @@
-// firebase-init.js
-import { firebaseConfig } from './firebase-config.js';
+import { db } from "./firebase-config.js";
+import { ref, set } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// Passer la config à l'initialisation Firebase intégrée dans index.html
-window.__gfFirebaseConfig = firebaseConfig;
+const workspace = "savigny-le-temple";
+
+// Création des catégories
+set(ref(db, `workspaces/${workspace}/categories`), {
+  ecriture: "Écriture",
+  rangement: "Rangement",
+  informatique: "Informatique"
+});
+
+// Exemple de fournitures
+set(ref(db, `workspaces/${workspace}/fournitures/id1`), {
+  nom: "Stylo bleu",
+  categorie: "ecriture",
+  stock: 12,
+  seuil: 5,
+  description: "Stylo bille bleu"
+});
+
+set(ref(db, `workspaces/${workspace}/fournitures/id2`), {
+  nom: "Classeur A4",
+  categorie: "rangement",
+  stock: 4,
+  seuil: 2,
+  description: "Classeur cartonné A4"
+});
